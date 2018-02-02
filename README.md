7663805df32865cadae35a51d62b6e147141ecb0
Hai Minh Nguyen <nguyen.minh.hai@live.com>
2018-02-02 15:26:46 +0700

    update scripts

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Samples for Docker images at https://github.com/katalon-studio/docker-images.
+
+# Katalon Studio
+
+    cd <project_root_directory>
+    ./katalon/run_chrome.sh
+    ./katalon/run_firefox.sh
--- katalon/run_chrome.sh
+++ katalon/run_chrome.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-rm -rfv /home/ubuntu/report/chrome
-mkdir -p /home/ubuntu/report/chrome
+rm -rfv ./reports/chrome
+mkdir -p ./reports/chrome
 
 katalon_opts='-browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"'
-docker run --rm -v /home/ubuntu/katalon-test:/katalon/katalon/source:ro -v /home/ubuntu/report/chrome:/katalon/katalon/report -e KATALON_OPTS="$katalon_opts" katalonstudio/katalon
+docker run --rm -v ./katalon:/katalon/katalon/source:ro -v ./reports/chrome:/katalon/katalon/report -e KATALON_OPTS="$katalon_opts" katalonstudio/katalon
