1f793d127cb321b9e642c8dc80198ba9a25b09b5
Hai Minh Nguyen <nguyen.minh.hai@live.com>
2018-02-02 15:29:10 +0700

    update scripts

--- katalon/run_chrome.sh
+++ katalon/run_chrome.sh
@@ -4,4 +4,4 @@ rm -rfv ./reports/chrome
 mkdir -p ./reports/chrome
 
 katalon_opts='-browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"'
-docker run --rm -v katalon:/katalon/katalon/source:ro -v reports/chrome:/katalon/katalon/report -e KATALON_OPTS="$katalon_opts" katalonstudio/katalon
+docker run --rm -v $(pwd)/katalon:/katalon/katalon/source:ro -v $(pwd)/reports/chrome:/katalon/katalon/report -e KATALON_OPTS="$katalon_opts" katalonstudio/katalon
