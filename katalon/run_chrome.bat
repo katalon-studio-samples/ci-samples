d928b0e582c80120bc4289065ac1919cf62cabb5
Alex Smith <dev.alex.88@gmail.com>
2018-07-03 12:45:12 +0700

    update windows batch file

--- katalon/run_chrome.bat
+++ katalon/run_chrome.bat
@@ -1,13 +1,15 @@
 @echo on
 
-set current_dir=%~dp0
-set project_dir="%current_dir%"
-set reports_dir="%project_dir%\..\reports\chrome"
+set current_dir=%cd%
+set script_dir=%~dp0
+set project_dir=%script_dir%.
+set reports_dir=%project_dir%\..\reports\chrome
 
-rem rm -rfv "$reports_dir"
-mkdir -p "%reports_dir%"
+del /f /s /q "%reports_dir%"
+rmdir /s /q "%reports_dir%"
+mkdir "%reports_dir%"
 
-rem katalon_opts='-browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"'
-rem docker run --rm -v "$project_dir":/katalon/katalon/source:ro -v "$reports_dir":/katalon/katalon/report -e KATALON_OPTS="$katalon_opts" katalonstudio/katalon
+set katalon_opts="-browserType=\"Chrome\" -retry=0 -statusDelay=15 -testSuitePath=\"Test Suites/TS_RegressionTest\""
+docker run --rm -v "%project_dir%":/katalon/katalon/source:ro -v "%reports_dir%":/katalon/katalon/report -e KATALON_OPTS=%katalon_opts% katalonstudio/katalon
 
-rem cd $current_dir
+cd "%current_dir%"
