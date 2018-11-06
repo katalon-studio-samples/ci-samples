3376a9ca0fd2234e6927c3cc2eab6b80d26f0453
Alex Smith <dev.alex.88@gmail.com>
2018-11-06 11:17:40 +0700

    update shell scripts

--- run_chrome.sh
+++ run_chrome.sh
@@ -2,4 +2,4 @@
 
 set -xe
 
-docker run -t --rm -v "$(pwd)":/katalon/katalon/source katalonstudio/katalon:test katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"
+docker run -t --rm -v "$(pwd)":/katalon/katalon/source katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"
